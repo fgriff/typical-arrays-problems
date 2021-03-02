07e4f019c5b33c15e0e5f82a109c9c4503b0a0aa
fgriff <>
2021-03-02 21:19:17 +0300

    feat: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+
+    return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+
+    return array.reduce((sum, item)=> sum + item, 0) / array.length;
 }
